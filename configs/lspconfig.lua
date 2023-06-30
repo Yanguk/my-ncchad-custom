af91ab1581c64477d42c7cd6ada2b6b5e4ddec72
Yanguk <yanguk.heo@knowre.com>
2023-07-01 08:47:40 +0900

    Update: eslint 및 null-ls 설정 추가

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -13,15 +13,36 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- 
--- lspconfig.pyright.setup { blabla}
+lspconfig.tsserver.setup {
+  init_options = {
+    hostInfo = "neovim",
+    preferences = {
+      importModuleSpecifierPreference = "non-relative",
+    },
+  },
+}
 
-lspconfig"tsserver".setup({
-	init_options = {
-		hostInfo = "neovim",
-		preferences = {
-			importModuleSpecifierPreference = "non-relative",
-		},
-	},
-})
+local eslint_settings = {}
 
+local function has_yarn_folder()
+  local yarn_path = vim.fn.getcwd() .. "/.yarn"
+  return vim.fn.isdirectory(yarn_path) == 1
+end
+
+if has_yarn_folder() then
+  eslint_settings = {
+    nodePath = vim.fn.getcwd() .. "/.yarn/sdks",
+    packageManager = "yarn",
+  }
+end
+
+lspconfig.eslint.setup {
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+  settings = eslint_settings,
+}
--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -10,13 +10,24 @@ local sources = {
 
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "typesript" } }, -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
 
   -- cpp
   b.formatting.clang_format,
+
+  -- custom
+  b.formatting.rustfmt,
+  b.code_actions.gitsigns,
+  b.code_actions.eslint,
+  b.diagnostics.cspell.with {
+    extra_args = { "--config", "~/.config/cspell.json" },
+  },
+  b.code_actions.cspell.with {
+    extra_args = { "--config", "~/.config/cspell.json" },
+  },
 }
 
 null_ls.setup {

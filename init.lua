21176dec4e3e34fc35c7acf2b14b9876e51b54be
Yanguk <yanguk.heo@knowre.com>
2023-07-03 08:31:55 +0900

    Update: autoResize 삭제

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
-local util = require "custom.configs.util"
+-- local util = require "custom.configs.util"
 
-local autocmd = vim.api.nvim_create_autocmd
+-- local autocmd = vim.api.nvim_create_autocmd
 -- Auto resize panes when resizing nvim window
-autocmd("VimResized", {
-  pattern = "*",
-  command = "tabdo wincmd =",
-})
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
 
 -- buffer
 -- vim.api.nvim_create_autocmd({ "BufAdd", "BufEnter", "tabnew" }, {
@@ -22,7 +22,7 @@ vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = false
 
 -- show trailing whitespaces
-util.toggle_trailing_space()
+-- util.toggle_trailing_space()
 
 vim.o.termguicolors = true
 vim.opt.expandtab = false

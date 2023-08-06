3a86b2be2319cdc9929f1e8030b277fd3eabeb30
Yanguk <yanguk.heo@knowre.com>
2023-08-06 11:23:34 +0900

    Update: cmp옵션 옮기고, pre-select 옵션 삭제

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -106,4 +106,18 @@ M.copilot = {
   },
 }
 
+M.cmp = {
+  completion = {
+    completeopt = "menu,menuone,noinsert,noselect",
+  },
+  sources = {
+    { name = "nvim_lsp", group_index = 2 },
+    { name = "copilot", group_index = 2 },
+    { name = "luasnip", group_index = 2 },
+    { name = "buffer", group_index = 2 },
+    { name = "nvim_lua", group_index = 2 },
+    { name = "path", group_index = 2 },
+  },
+}
+
 return M
--- plugins.lua
+++ plugins.lua
@@ -69,16 +69,7 @@ local plugins = {
       require "custom.configs.cmp"
       require("cmp").setup(opts)
     end,
-    opts = {
-      sources = {
-        { name = "nvim_lsp", group_index = 2 },
-        { name = "copilot", group_index = 2 },
-        { name = "luasnip", group_index = 2 },
-        { name = "buffer", group_index = 2 },
-        { name = "nvim_lua", group_index = 2 },
-        { name = "path", group_index = 2 },
-      },
-    },
+    opts = overrides.cmp,
   },
 
   {

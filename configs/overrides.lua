d592fe47c641e9623fb0d1757062300e8b760091
Yanguk <yanguk.heo@knowre.com>
2023-09-06 21:25:11 +0900

    copilot 순서 변경

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -114,8 +114,8 @@ M.copilot = {
 -- for copilot-cmp
 M.cmp = {
   sources = {
-    { name = "nvim_lsp", group_index = 2 },
     { name = "copilot", group_index = 2 },
+    { name = "nvim_lsp", group_index = 2 },
     { name = "luasnip", group_index = 2 },
     { name = "buffer", group_index = 2 },
     { name = "nvim_lua", group_index = 2 },

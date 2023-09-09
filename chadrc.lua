24068332d0497130dc24ed94fd9d9e8615f8efdf
Yanguk <yanguk.heo@knowre.com>
2023-09-09 13:17:51 +0900

    Update: catppucchin 써보기

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "gruvbox",
-  theme_toggle = { "gruvbox", "one_dark" },
+  theme = "catppuccin",
+  theme_toggle = { "catppuccin", "one_dark" },
   -- transparency = true,
 
   hl_override = highlights.override,

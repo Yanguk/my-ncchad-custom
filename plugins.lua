770462ad6647f350806683c9913365adb645a09e
Yanguk <yanguk.heo@knowre.com>
2024-02-08 11:52:05 +0900

    Add: Octo

--- plugins.lua
+++ plugins.lua
@@ -288,12 +288,22 @@ local plugins = {
       "rfc_semicolon",
     },
     event = "BufRead",
-    -- cmd = {
-    --   "RainbowDelim",
-    --   "RainbowDelimSimple",
-    --   "RainbowDelimQuoted",
-    --   "RainbowMultiDelim",
-    -- },
+  },
+
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup { enable_builtin = true }
+      vim.cmd [[hi OctoEditable guibg=none]]
+    end,
+    keys = {
+      { "<leader>O", "<cmd>Octo<cr>", desc = "Octo" },
+    },
   },
 }
 

0c3f1a7b14aa26c5787a504fcd627bb3e46b476f
Yanguk <yanguk.heo@knowre.com>
2024-03-06 22:31:19 +0900

    Update: lsp 설정 중복 제거

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,78 +1,78 @@
+---@diagnostic disable-next-line: deprecated
+table.unpack = table.unpack or unpack
+
 local lspconfig = require "lspconfig"
 
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
+local plugins_lsp_config = require "plugins.configs.lspconfig"
+
+local on_attach = plugins_lsp_config.on_attach
+local capabilities = plugins_lsp_config.capabilities
+local on_init = plugins_lsp_config.on_init
+
 -- nvim-ufo
 capabilities.textDocument.foldingRange = {
   dynamicRegistration = false,
   lineFoldingOnly = true,
 }
 
-local util = require "custom.configs.util"
-
-lspconfig["tsserver"].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  init_options = {
-    preferences = {
-      importModuleSpecifierPreference = "non-relative",
-    },
-  },
-}
-
-lspconfig["eslint"].setup {
-  on_attach = function(client, bufnr)
-    on_attach(client, bufnr)
-
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      command = "EslintFixAll",
-    })
-  end,
-  capabilities = capabilities,
-  settings = util.eslint_settings,
-}
-
-lspconfig["bashls"].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "sh", "zsh", "bash" },
-}
-
-lspconfig["yamlls"].setup {
+local default_config = {
+  on_init = on_init,
   on_attach = on_attach,
   capabilities = capabilities,
 }
 
-lspconfig["clangd"].setup {
-  on_attach = on_attach,
-  capabilities = vim.tbl_extend("force", capabilities, {
-    offsetEncoding = "utf-16",
-  }),
-}
+local server_configs = {
+  "yamlls",
 
-vim.g.rustaceanvim = {
-  -- Plugin configuration
-  tools = {},
-  -- LSP configuration
-  server = {
-    on_attach = on_attach,
-    settings = {
-      -- rust-analyzer language server configuration
-      ["rust-analyzer"] = {
-        command = "clippy",
-        -- overrideCommand = {
-        --   "cargo",
-        --   "clippy",
-        --   "--fix",
-        --   "--workspace",
-        --   "--message-format=json",
-        --   "--all-targets",
-        --   "--allow-dirty",
-        -- },
+  ["tsserver"] = {
+    init_options = {
+      preferences = {
+        importModuleSpecifierPreference = "non-relative",
       },
     },
   },
-  -- DAP configuration
-  dap = {},
+  ["eslint"] = {
+    on_attach = function(client, bufnr)
+      on_attach(client, bufnr)
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        command = "EslintFixAll",
+      })
+    end,
+    settings = require("custom.configs.util").eslint_settings,
+  },
+  ["bashls"] = {
+    filetypes = { "sh", "zsh", "bash" },
+  },
+  ["clangd"] = {
+    capabilities = vim.tbl_extend("force", capabilities, {
+      offsetEncoding = "utf-16",
+    }),
+  },
+  ["rust_analyzer"] = {
+    settings = {
+      ["rust-analyzer"] = {
+        command = "clippy",
+      },
+    },
+  },
 }
+
+for k, v in pairs(server_configs) do
+  local server, config = table.unpack(
+    type(k) == "number" and { v, default_config } or { k, vim.tbl_deep_extend("force", default_config, v) }
+  )
+
+  if server == "rust_analyzer" then
+    vim.g.rustaceanvim = {
+      -- Plugin configuration
+      tools = {},
+      -- LSP configuration
+      server = config,
+      -- DAP configuration
+      dap = {},
+    }
+  else
+    lspconfig[server].setup(config)
+  end
+end
